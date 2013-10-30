48fbcbfec4ce7dfd4ecc6f782ff7f746a37beb8e
s198544 <monaandresen@Monas-MacBook-Pro.local>
2013-10-30 12:36:49 +0100

    Index endringer
    Jeg la til noen sider til på index siden

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+include "faste_elementer/toolbar.html";
+include "stil.html";
+
+
+if (empty($_GET["page"]))
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="1")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="2")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="3")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="4")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="5")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="6")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="7")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="8")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="9")
+{
+	include("eksempel.php");
+}
+
+elseif($_GET["page"]=="10")
+{
+	include("eksempel.php");
+}
+
+
+
+else
+{
+	include("?page1.php");
+}
+
+
+include ("faste_elementer/footer.html");
+
+
+
+?>
